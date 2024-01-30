6561f27657ba3b084b1a9e10c3e3744a1c4fdc65
VBGupta13 <121970537+VBGupta13@users.noreply.github.com>
2024-01-30 11:58:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,169 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import base64
+st.set_page_config(layout="wide")
+
+def show_pdf(file_path):
+        with open(file_path,"rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="400" height="600" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
+st.write('<style>div.block-container{padding-top:0rem;}</style>', unsafe_allow_html=True)
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="GUPTA",
+        options=["Home","Experience", "Education", "Skills", "Resume","Contact"],
+        icons=["house", "clock history","book half", "tools", "paperclip","envelope"],
+        menu_icon="none",
+        default_index=0,
+    )
+
+if selected == "Home":
+    st.markdown("<div style='text-align:center;margin-top:20px'><h1>ABOUT ME</h1></div>",unsafe_allow_html=True)
+    with st.container():
+        matter, photo = st.columns(2)
+        with matter:
+            st.markdown("<div style='text-align:center;margin-top:80px'></div>",unsafe_allow_html=True)
+            st.write("<div style='font-size: 18px;'>Hi, I'm VEERA BHADRA GUPTA CHARUGALLA. I'm currently pursuing B.Tech 4th year in the department of Computer Science and Engineering at Sri Vasavi Engineering College, Pedatadepalli affiliated to JNTUK</div>", unsafe_allow_html=True)
+            st.text("")
+            st.write("<div style='font-size: 18px;'>▶ In addition I'd like to play table tennis 🏓 , badminton, listening music 🎧 and...  enjoy eating food 🍽️ in my free time!</div>", unsafe_allow_html=True)
+            st.text("")
+            st.markdown("<div><h4>Social Media</h4></div>",unsafe_allow_html=True)
+            st.markdown("""
+                -  [linkedIN](www.linkedin.com/in/veera-bhadra-gupta-charugalla-b0b994245)
+                -  [Github](https://github.com/VBGupta13/)
+                """)
+            st.markdown("<div><h4>Resume</h4></div>",unsafe_allow_html=True)
+            st.markdown("""
+                -  [Resume](https://drive.google.com/file/d/1jDk641XVjRTc5wtMz7C7wCg75nVJKJNQ/view?usp=drive_link)
+                """)
+        photo.image("gupta.jpg")
+elif selected == "Experience":
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>EXPERIENCE</h1></div>",unsafe_allow_html=True)
+    left,right = st.columns(2)
+    with left:
+        st.markdown("<div style='text-align:center;margin-top:50px'></div>",unsafe_allow_html=True)
+        image_column, text_column = st.columns((1,5))
+        with image_column:
+            st.image("Oasis.jpeg")
+        with text_column:
+            st.subheader("Web Developer , Oasis Infobyte")
+            st.write("*01-2023 to 02-2023*")
+            st.markdown("""
+             -  Developed landing page , portfolio , temprature converter by using 
+            HTML,CSS,JAVASCRIPT.
+            """)
+    with right:
+        st.markdown("<div style='text-align:center;margin-top:50px'></div>",unsafe_allow_html=True)
+        image_column, text_column = st.columns((1,5))
+        with image_column:
+            st.image("Edunet.png")
+        with text_column:
+            st.subheader("Web Developer , Edunet foundation Intern")
+            st.write("*01-2023 to 02-2023*")
+            st.markdown("""
+             -   Developed login authentication , quiz website by using 
+            HTML,CSS,JAVASCRIPT.
+            """)
+
+elif selected == "Skills":
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>SKILLS</h1></div>",unsafe_allow_html=True)
+    
+    with st.container():
+        left,middle,right = st.columns(3)
+        with left:
+            with st.container():
+                html = 70
+                st.markdown("HTML")
+                st.progress(html)
+                st.write(html,"%")
+                st.text("")
+            with st.container():
+                python = 50
+                st.markdown("PYTHON")
+                st.progress(python)
+                st.write(python,"%")
+                st.text("")
+            with st.container():
+                cp = 70
+                st.markdown("C++")
+                st.progress(cp)
+                st.write(cp,"%")
+                st.text("")
+           
+        with middle:
+            with st.container():
+                css = 70
+                st.markdown("CSS")
+                st.progress(css)
+                st.write(css,"%")
+                st.text("")
+            with st.container():
+                sql = 80
+                st.markdown("SQL")
+                st.progress(sql)
+                st.write(sql,"%")
+                st.text("")
+        with right:
+            with st.container():
+                java = 80
+                st.markdown("JAVA")
+                st.progress(java)
+                st.write(java,"%")
+                st.text("")
+            with st.container():
+                c = 70
+                st.markdown("C")
+                st.progress(c)
+                st.write(c,"%")
+                st.text("")
+elif selected == "Contact":
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>FEEDBACK FORM</h1></div>",unsafe_allow_html=True)
+    with st.form("Form1"):
+        col1,col2 = st.columns(2)
+        firstname = col1.text_input("First Name :")
+        lastname = col2.text_input("Last Name :")
+        email = st.text_input("Email :")
+        mobile = st.text_input("Mobile Number")
+        textarea = st.text_area("Feedback")
+        state = st.form_submit_button("Submit",type="primary")
+
+    st.markdown("<div style='text-align:center;margin-top:10px'><h4>MAIL-ID : </h4><h6>guptach1105@gmail.com</h6></div>",unsafe_allow_html=True)
+    st.markdown("<div style='text-align:center;margin-top:10px'><h4>MOBILE MUMBER : </h4><h6>+91-93929-68736</h6></div>",unsafe_allow_html=True)
+    if state:
+        if firstname == "" or lastname == "" or email == "" or mobile == "" or textarea == "":
+            st.warning("please enter all the fields")
+        else:
+            st.success("Submited Successfully")
+elif selected == "Education":
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>EDUCATION</h1></div>",unsafe_allow_html=True)
+    
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.image("school1.jpg", use_column_width=True)
+            st.markdown("<div align='center'><b>Vasavi Ideal Public School</b> <br> <b>High School</b> <br> <b>Percentage - 77.2%</b> </div>", unsafe_allow_html=True)
+            st.text("")
+
+        with col2:
+            st.image("inter.jpg", use_column_width=True)
+            st.markdown("<div align='center'><b>Tirumala Junior College</b> <br> <b>Intermediate</b> <br> <b>Percentage - 93.7%</b> </div>", unsafe_allow_html=True)
+            st.text("")
+
+        with col3:
+            st.image("Sri.jpeg", use_column_width=True)
+            st.markdown("<div align='center'><b>Sri Vasavi Engineering College</b>  <br> <b> Bachelor of Technology</b> <br> <b>Computer Science and Engineering</b> <br> <b>Percentage - 79.34%</b></div>", unsafe_allow_html=True)
+            st.text("")
+elif selected == "Resume":   
+    
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>RESUME</h1></div>",unsafe_allow_html=True)
+    
+    with open("Veera_Bhadra_Gupta_Charugalla.pdf", "rb") as file:
+        btn = st.download_button(
+            label="Download Resume",
+            data=file,
+            file_name="Veera_Bhadra_Gupta_Charugalla.pdf",
+            mime="application/pdf",type="primary"
+        )
